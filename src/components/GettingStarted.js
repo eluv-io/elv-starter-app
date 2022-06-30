9746bfa970c6ccf8241ef5cf5efba9c3dfe60d37
elv-seth <107427344+elv-seth@users.noreply.github.com>
2022-06-30 13:59:30 -0700

    Update src/components/GettingStarted.js
    Co-authored-by: Zenia Villa <91760982+elv-zenia@users.noreply.github.com>

--- src/components/GettingStarted.js
+++ src/components/GettingStarted.js
@@ -7,7 +7,7 @@ const GettingStarted = ({Login}) => {
     <div >
       <img src={logo} />
       <div className="spacer">
-        <Action label="login"title="Login"type="button"className="primarybtn"onClick={Login}children={<p className="btntext">Login</p>} />
+        <Action label="login" title="Login" type="button" className="primarybtn" onClick={Login} children={<p className="btntext">Login</p>} />
      </div>  
     </div>
   );
