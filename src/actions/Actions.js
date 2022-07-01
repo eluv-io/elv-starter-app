3b7ad13f52031ed60f9dabb18504b29b8f513cdf
Seth Carlson <sethcarlson@Seths-MacBook-Pro.local>
2022-07-01 13:58:36 -0700

    removed unused files

--- src/actions/Actions.js
+++
@@ -1,9 +0,0 @@
-import dispatcher from "../dispatcher";
-
-export function loginAction() {
-    dispatcher.dispatch({type:"LOG_IN"});
-}
-
-export function logoutAction() {
-    dispatcher.dispatch({type:"LOG_OUT"});
-}
